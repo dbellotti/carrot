8e5eeaf4f9882879a31b2c16a4ee0e284608508e
David Bellott and Ian Ornstein <dbellotti@pivotal.io>
2015-01-27 17:20:35 -0800

    Quiets errors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+
   def index
     WelcomeMailer.invite('dbellotti@pivotal.io').deliver_now
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -77,12 +77,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '25',
-    :domain         => 'run.pivotal.io',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD']
+    :address              => 'smtp.sendgrid.net',
+    :port                 => '587',
+    :domain               => ENV['DOMAIN'],
+    :authentication       => :plain,
+    :enable_starttls_auto => true,
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD']
   }
 end
